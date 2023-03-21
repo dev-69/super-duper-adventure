d1fe29340de82698ee1eea14614e5c93f1f0203a
Manthan997 <manthanparsekar997@gmail.com>
2023-03-21 14:28:20 +0530

    left to implement message sending part. emergency response

--- emergency_lock/emergency_lock.ino
+++ emergency_lock/emergency_lock.ino
@@ -1,6 +1,7 @@
 //BOARD PIN SETTING
 //board considered: UNO
 int reed_lock_pin = 13;  //for uno
+int buzzer_pin = 12;  //for uno
 
 
 
@@ -15,13 +16,27 @@ bool real_lock_status;                    //1 is open and 0 is closed
 //function for checking the real lock status
 void reedLockCheck()
 {
-//  declare the reading pin as input
-pinMode(reed_lock_pin, INPUT);
-//update the global variable directly
-real_lock_status = digitalRead(reed_lock_pin);
-
-//try using return type and static function for making code more secure
+    //  declare the reading pin as input
+    pinMode(reed_lock_pin, INPUT);
+    //update the global variable directly
+    real_lock_status = digitalRead(reed_lock_pin);
+    
+    //try using return type and static function for making code more secure
  
+}
+
+//function for buzzer ringing
+void buzzerWithDelay(int time)
+{
+//   declare the buzzerpin as output
+   pinMode(buzzer_pin, OUTPUT);
+   for(int i = 0;i < time; i++)
+   {
+      digitalWrite(buzzer_pin,HIGH);
+      delay(498);
+      digitalWrite(buzzer_pin,LOW);
+      delay(498);
+   }
 }
 
 //implementing the conditional statement
@@ -29,23 +44,26 @@ real_lock_status = digitalRead(reed_lock_pin);
 int doorStatusUpdate()
 // global variables used hence no arguments passed to function
 {
-//  it is good to update the varibles as this function starts.
-//  call reedLockCheck function
-void reedLockCheck();
-  if(virtual_lock_status==1 && real_lock_status==0)     //1 is open and 0 is closed
-  {
-//    the real door is closed and needs to be updated into program
-    virtual_lock_status = 0;
-  }
-  else if(virtual_lock_status==0 && real_lock_status==1)
-  {
-//    emergency mode triggered
-
-  }
-  else
-  {
-//    includes conditions where both variables are same. hence, system is stable and do nothin here 
-  }
+    //  it is good to update the varibles as this function starts.
+    //  call reedLockCheck function
+    void reedLockCheck();
+      if(virtual_lock_status==1 && real_lock_status==0)     //1 is open and 0 is closed
+      {
+        //    the real door is closed and needs to be updated into program
+            virtual_lock_status = 0;
+      }
+      else if(virtual_lock_status==0 && real_lock_status==1)
+      {
+        //    emergency mode triggered
+        
+      }
+      else
+      {
+        //    includes conditions where both variables are same. hence, system is stable and do nothin here 
+        void buzzerWithDelay(30);  //the argument includes seconds of delay
+        //    send message to owner
+        // send live recording part
+      }
 }
 
 void setup() {
